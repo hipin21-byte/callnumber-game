ccd303a6110a639321a911fb532167f9bc5f5146
hipin21-byte <hipin21@gmail.com>
2026-09-11 14:45:33 +0900

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 // Google Apps Script 웹앱 배포 후 아래 주소를 입력하세요.
 // 예: https://script.google.com/macros/s/AKfycb.../exec
-window.CALLNUMBER_API_URL = "https://script.google.com/macros/s/AKfycbwJNCc16nBIaFI5BMF8kLRArJSjkJ3pI8dyNa_QH2GXo0TzgBjxdEYs2TWVOyppBBCmAA/exec";
+window.CALLNUMBER_API_URL = "https://script.google.com/macros/s/AKfycbyMNmVBn4U4WndZpwhovd-YMR4fhZopULlcg7ngTPWJfpYFco5lcI7trj8eFiCSz3zh4A/exec";
